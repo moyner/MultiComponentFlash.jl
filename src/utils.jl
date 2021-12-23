d28ec007ffcd75a32b7bfcaed151a0c325e9a0f6
Olav Møyner <olav.moyner@gmail.com>
2021-12-23 20:57:13 +0100

    Clean up LBC viscosity
    Remove broadcasts, add promote_type

--- src/utils.jl
+++ src/utils.jl
@@ -23,12 +23,12 @@ end
 
 Compute the viscosity of a mixture using the [Lohrenz-Bray-Clark](https://doi.org/10.2118/915-PA) correlation.
 """
-function lbc_viscosity(eos, p, T, ph; coeff = (0.1023, 0.023364, 0.058533, -0.040758, 0.0093324), shift = -1e-4)
+function lbc_viscosity(eos, p, temperature, ph; coeff = (0.1023, 0.023364, 0.058533, -0.040758, 0.0093324), shift = -1e-4)
     molfactor = 1000;
     Rankine = 5/9
     psia = 6.894757293168360e+03
     
-    rho = 1/molar_volume(eos, p, T, ph)
+    rho = 1/molar_volume(eos, p, temperature, ph)
     z = ph.mole_fractions
     
     properties = eos.mixture.properties
@@ -40,7 +40,7 @@ function lbc_viscosity(eos, p, T, ph; coeff = (0.1023, 0.023364, 0.058533, -0.04
 
     a = 0.0
     b = 0.0
-    for i in eachindex(properties)
+    @inbounds for i in eachindex(properties)
         prop = properties[i]
         zi = z[i]
         mw = prop.mw
@@ -54,37 +54,36 @@ function lbc_viscosity(eos, p, T, ph; coeff = (0.1023, 0.023364, 0.058533, -0.04
         P_pc += zi*p_c
         T_pc += zi*T_c
         Vc += zi*V_c
+        # Scale to right units
+        tr = temperature/T_c
+        Tc = T_c/Rankine
+        Pc = p_c/psia
 
-
-        tr = T/T_c
-        Tc = T_c/Rankine;
-        Pc = p_c/psia;
-
-        e_i = (5.4402*Tc.^(1/6))./(mwi.*Pc.^(2/3).*(1e-3));
+        e_i = (5.4402*Tc^(1/6))/(mwi*Pc^(2/3)*(1e-3))
         
-        large = tr > 1.5;
+        large = tr > 1.5
         if large
-            mu_i = (17.78e-5.*(4.58*tr - 1.67).^0.625)./e_i;
+            mu_i = (17.78e-5*(4.58*tr - 1.67)^0.625)/e_i
         else
-            mu_i = 34e-5.*tr.^(0.94)./e_i;
+            mu_i = 34e-5*tr^(0.94)/e_i
         end
-        
-        a = a + zi.*mu_i.*mwi;
-        b = b + zi.*mwi;
+        a = a + zi*mu_i*mwi
+        b = b + zi*mwi
     end
-    mu_atm = a./b;
-    e_mix = 5.4402*(T_pc./Rankine).^(1/6)./((molfactor*mwc).^(1/2).*(P_pc./psia).^(2/3).*(1e-3));
-    rhor = Vc.*rho;
+    mu_atm = a/b
+    e_mix = 5.4402*(T_pc/Rankine)^(1/6)/((molfactor*mwc)^(1/2)*(P_pc/psia)^(2/3)*(1e-3))
+    rhor = Vc*rho
     # From Jossi et al
-    # coeffs = [0.1023, 0.023364, 0.058533, -0.040758, 0.0093724, 1e-4];
+    # coeffs = [0.1023, 0.023364, 0.058533, -0.040758, 0.0093724, 1e-4]
     # From LBC paper
-    # coeffs = [0.1023, 0.023364, 0.058533, -0.040758, 0.0093324, -1e-4];
+    # coeffs = [0.1023, 0.023364, 0.058533, -0.040758, 0.0093324, -1e-4]
     corr = 0.0
     for (i, c) in enumerate(coeff)
         corr += c*rhor^(i-1)
     end
-    mu = mu_atm + (corr^4 + shift)./e_mix;
-    return mu
+    mu = mu_atm + (corr^4 + shift)/e_mix
+    T = Base.promote_type(typeof(p), typeof(temperature))
+    return mu::T
 end
 
 function molar_volume(R, p, T, Z)
