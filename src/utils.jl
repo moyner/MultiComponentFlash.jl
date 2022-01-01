01bdc8a5ce3478394e6153630d6fbbb263b719e7
Olav Møyner <olav.moyner@gmail.com>
2022-01-01 21:20:16 +0100

    Split LBC into multiple functions
    + make conversion factors explicit & const
    + fix small bug

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,8 @@
+const CENTI_POISE_TO_PASCAL_SECOND = 1000.0
+const MOL_TO_KMOL = 1000.0
+const KELVIN_TO_RANKINE = 5/9
+const PASCAL_TO_PSI = 1.450377377302092e-04
+
 """
     single_phase_label(mixture, cond)
 
@@ -24,47 +29,15 @@ end
 Compute the viscosity of a mixture using the [Lohrenz-Bray-Clark](https://doi.org/10.2118/915-PA) correlation.
 """
 function lbc_viscosity(eos, p, temperature, ph::FlashedPhase{T}; coeff = (0.1023, 0.023364, 0.058533, -0.040758, 0.0093324), shift = -1e-4) where T
-    molfactor = 1000
-    Rankine = 5/9
-    psia = 6.894757293168360e+03
     
     rho = 1/molar_volume(eos, p, temperature, ph)
     z = ph.mole_fractions
     
     properties = eos.mixture.properties
-    P_pc = zero(T)
-    T_pc = zero(T)
-    Vc = zero(T)
-    mwc = zero(T)
-
-    a = zero(T)
-    b = zero(T)
-    @inbounds for (prop, zi) in zip(properties, z)
-        mw = molfactor*prop.mw
-        T_c = prop.T_c/Rankine
-        p_c = prop.p_c/psia
-        V_c = prop.V_c
-        # Accumulate weighted properties
-        mwc += zi*mw
-        P_pc += zi*p_c
-        T_pc += zi*T_c
-        Vc += zi*V_c
-        # Add contributions to atmospheric mu
-        tr = temperature/T_c
-        mwi = sqrt(mw)
-        e_i = (5.4402*T_c^(1/6))/(mwi*p_c^(2/3)*(1e-3))
-        large = tr > 1.5
-        if large
-            mu_i = (17.78e-5*(4.58*tr - 1.67)^0.625)/e_i
-        else
-            mu_i = 34e-5*tr^(0.94)/e_i
-        end
-        a += zi*mu_i*mwi
-        b += zi*mwi
-    end
-    mu_atm = a/b
-    e_mix = 5.4402*(T_pc)^(1/6)/((mwc)^(1/2)*(P_pc)^(2/3)*(1e-3))
-    rhor = Vc*rho
+    mw_mix, P_pc, T_pc, V_pc = pseudo_critical_properties(properties, z)
+    mu_atm = atmospheric_mu_estimate(properties, z, temperature)
+    e_mix = mixture_viscosity_parameter(mw_mix, T_pc, P_pc)
+    rhor = V_pc*rho
     # From Jossi et al
     # coeffs = [0.1023, 0.023364, 0.058533, -0.040758, 0.0093724, 1e-4]
     # From LBC paper
@@ -75,6 +48,50 @@ function lbc_viscosity(eos, p, temperature, ph::FlashedPhase{T}; coeff = (0.1023
     end
     mu = mu_atm + (corr^4 + shift)/e_mix
     return mu::T
+end
+
+function atmospheric_mu_estimate(props, z, temperature)
+    T = eltype(z)
+    a = zero(T)
+    b = zero(T)
+    @inbounds for (prop, zi) in zip(props, z)
+        mw = prop.mw
+        T_c = prop.T_c
+        p_c = prop.p_c
+        # Add contributions to atmospheric mu
+        T_r = temperature/T_c
+        e_i = mixture_viscosity_parameter(mw, T_c, p_c)
+        if T_r > 1.5
+            mu_i = 17.78e-5*(4.58*T_r - 1.67)^0.625
+        else
+            mu_i = 34e-5*T_r^(0.94)
+        end
+        tmp = sqrt(mw)*zi
+        a += tmp*mu_i/e_i
+        b += tmp
+    end
+    return a/b
+end
+
+@inline function mixture_viscosity_parameter(molar_mass, T_c, p_c)
+    mwi = sqrt(MOL_TO_KMOL*molar_mass)
+    return CENTI_POISE_TO_PASCAL_SECOND*(5.4402*(T_c*KELVIN_TO_RANKINE)^(1/6))/(mwi*(p_c*PASCAL_TO_PSI)^(2/3))
+end
+
+function pseudo_critical_properties(props, z)
+    T = eltype(z)
+    P_pc = zero(T)
+    T_pc = zero(T)
+    Vc = zero(T)
+    mwc = zero(T)
+    @inbounds for (prop, zi) in zip(props, z)
+        # Accumulate weighted properties
+        mwc += zi*prop.mw
+        P_pc += zi*prop.p_c
+        T_pc += zi*prop.T_c
+        Vc += zi*prop.V_c
+    end
+    return (mwc, P_pc, T_pc, Vc)
 end
 
 @inline function molar_volume(R, p, T, Z)
