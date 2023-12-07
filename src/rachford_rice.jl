b592d78e2b2bd6fa4b08c5b3623d407bfa4e879c
Olav Møyner <olav.moyner@gmail.com>
2023-12-07 11:37:45 +0100

    Clean up Rachford rice docs and code

--- src/rachford_rice.jl
+++ src/rachford_rice.jl
@@ -8,9 +8,11 @@ Compute vapor mole fraction `V` for given equilibrium constants `K` and mole fra
 `z` - Mole fractions. Should sum up to unity.
 
 # Keyword arguments
-`tol = 1e-12` - Tolerance for solve.
-`maxiter` - Maximum number of iterations
-`ad` - Use automatic differentiation (ForwardDiff) instead of analytical gradient.
+
+- `tol = 1e-12`: Tolerance for solve.
+- `maxiter=1000`: Maximum number of iterations
+- `ad=false`: Use automatic differentiation (ForwardDiff) instead of analytical gradient.
+- `analytical=true`: Use analytical solutions for 2 and 3 components.
 
 # Examples
 ```julia-repl
@@ -18,24 +20,16 @@ julia> solve_rachford_rice([0.5, 1.5], [0.3, 0.7])
 0.8000000000000002
 ```
 """
-function solve_rachford_rice(K, z, V = NaN; tol=1e-12, maxiter=1000, ad = false)
-    # Lowest bound for solution
-    V_min = 1/(1 - maximum(K))
-    # Largest bound for solution
-    V_max = 1/(1 - minimum(K))
-    if isnan(V)
-        V = (V_min + V_max)/2
-    end
-    i = 1
+function solve_rachford_rice(K, z, V = NaN; tol=1e-12, maxiter=1000, ad = false, analytical = false)
     n = length(z)
-    if n == 2 #exact solution, linear
+    if n == 2 && analytical #exact solution, linear
         z1,z2 = z
         k1,k2 = K
         b1,b2 = 1/(1-k1),1/(1-k2)
         a1 = (z1*b2 + z2*b1)
         a0 = z1+z2
         return a1/a0
-    elseif n == 3 #exact solution, quadratic
+    elseif n == 3 && analytical #exact solution, quadratic
         z1,z2,z3 = z
         k1,k2,k3 = K
         b1,b2,b3 = 1/(1-k1),1/(1-k2),1/(1-k3)
@@ -62,36 +56,45 @@ function solve_rachford_rice(K, z, V = NaN; tol=1e-12, maxiter=1000, ad = false)
         else
             return zero(Δ2)/zero(Δ2)
         end
-    end
-    while i < maxiter
-        if ad
-            f(V) = objectiveRR(V, K, z)
-            r = f(V)
-            Jo = ForwardDiff.derivative(f, V)
-            V = V - Jo\r
-        else
-            denum = 0.0
-            r = 0.0
-            @inbounds for i = 1:n
-                ΔK = K[i]-1
-                a = z[i]*ΔK
-                b = (1 + V*ΔK)
-                r += a/b
-                denum += z[i]*ΔK^2/b^2
+    else
+        # Lowest bound for solution
+        V_min = 1/(1 - maximum(K))
+        # Largest bound for solution
+        V_max = 1/(1 - minimum(K))
+        if isnan(V)
+            V = (V_min + V_max)/2
+        end
+        i = 1
+        while i < maxiter
+            if ad
+                f(V) = objectiveRR(V, K, z)
+                r = f(V)
+                Jo = ForwardDiff.derivative(f, V)
+                V = V - Jo\r
+            else
+                denum = 0.0
+                r = 0.0
+                @inbounds for i = 1:n
+                    ΔK = K[i]-1.0
+                    a = z[i]*ΔK
+                    b = (1.0 + V*ΔK)
+                    r += a/b
+                    denum += z[i]*ΔK^2/b^2
+                end
+                V = V + r/denum
             end
-            V = V + r/denum
+            if abs(r) < tol
+                break
+            end
+            if V < V_min || V > V_max
+                obj = (v) -> objectiveRR(v, K, z)
+                V = find_zero(obj, (V_min, V_max), Bisection())
+                break
+            end
+            i += 1
         end
-        if abs(r) < tol
-            break
-        end
-        if V < V_min || V > V_max
-            obj = (v) -> objectiveRR(v, K, z)
-            V = find_zero(obj, (V_min, V_max), Bisection())
-            break
-        end
-        i += 1
+        return V
     end
-    return V
 end
 
 function objectiveRR(V, K, z)
