3d9cdf9bd318f9011cf15420bd6c01105695ab2b
Olav Møyner <olav.moyner@gmail.com>
2025-08-09 14:12:32 +0200

    Update soave_redlich_kwong.jl

--- src/eos/soave_redlich_kwong.jl
+++ src/eos/soave_redlich_kwong.jl
@@ -6,11 +6,3 @@ function weight_ai(eos::GenericCubicEOS{SoaveRedlichKwong}, cond, i)
     T_r = reduced_temperature(mix, cond, i)
     return eos.ω_a*(1 + (0.48 + 1.574*a - 0.176*a^2)*(1-T_r^(1/2)))^2;
 end
-# SoaveRedlichKwong
-function weight_ai(eos::GenericCubicEOS{SoaveRedlichKwong}, cond, i)
-    mix = eos.mixture
-    m = molecular_property(mix, i)
-    a = acentric_factor(m)
-    T_r = reduced_temperature(mix, cond, i)
-    return eos.ω_a*(1 + (0.48 + 1.574*a - 0.176*a^2)*(1-T_r^(1/2)))^2;
-end
